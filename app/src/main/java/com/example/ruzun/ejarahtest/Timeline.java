5b1b90b104378b18af28a63709275f02fbb17083
ruzun <razanalsmari@gmail.com>
2020-02-16 19:29:26 +0300

    function update

--- app/src/main/java/com/example/ruzun/ejarahtest/Timeline.java
+++ app/src/main/java/com/example/ruzun/ejarahtest/Timeline.java
@@ -70,14 +70,8 @@ public class Timeline extends AppCompatActivity {
 
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
             return;
         }
 
