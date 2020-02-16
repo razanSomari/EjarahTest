f75fad63b4ad3ff5e8706964b43ba356a7fba708
ruzun <razanalsmari@gmail.com>
2020-02-16 18:44:40 +0300

    User location

--- app/src/main/java/com/example/ruzun/ejarahtest/Timeline.java
+++ app/src/main/java/com/example/ruzun/ejarahtest/Timeline.java
@@ -1,6 +1,9 @@
 package com.example.ruzun.ejarahtest;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,35 +13,44 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.sql.Time;
 import java.util.ArrayList;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class Timeline extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+        requestPermission();
+
         final ArrayList<Post> posts = new ArrayList<Post>();
-        posts.add(new Post("HELLOOOOO","anyone has a calcluse book?",20));
-        posts.add(new Post("Marrihan","I need a phone charger",88));
-        posts.add(new Post("jawaher","anyone interrested in creating apps?",23));
-        posts.add(new Post("amerah","welkfjlekrqkrmqkmwe",20));
-        posts.add(new Post("Mohammed","esgfafeqr qr r ",20));
-        posts.add(new Post("Sarah","qr qr qr ",20));
-        posts.add(new Post("Mona","qrr qrgt",20));
-        posts.add(new Post("Razan","anyone has a calcluse book?",20));
-        posts.add(new Post("Marrihan","I need a phone charger",88));
-        posts.add(new Post("jawaher","anyone interrested in creating apps?",23));
-        posts.add(new Post("Mohammed","welkfjlekrqkrmqkmwe",20));
+        posts.add(new Post("HELLOOOOO", "anyone has a calcluse book?", 20));
+        posts.add(new Post("Marrihan", "I need a phone charger", 88));
+        posts.add(new Post("jawaher", "anyone interrested in creating apps?", 23));
+        posts.add(new Post("amerah", "welkfjlekrqkrmqkmwe", 20));
+        posts.add(new Post("Mohammed", "esgfafeqr qr r ", 20));
+        posts.add(new Post("Sarah", "qr qr qr ", 20));
+        posts.add(new Post("Mona", "qrr qrgt", 20));
+        posts.add(new Post("Razan", "anyone has a calcluse book?", 20));
+        posts.add(new Post("Marrihan", "I need a phone charger", 88));
+        posts.add(new Post("jawaher", "anyone interrested in creating apps?", 23));
+        posts.add(new Post("Mohammed", "welkfjlekrqkrmqkmwe", 20));
 
 
-
-        PostAdapter<Post> adapter = new PostAdapter <Post>(this,  posts);
+        PostAdapter<Post> adapter = new PostAdapter<Post>(this, posts);
 
         ListView listView = (ListView) findViewById(R.id.post_list);
 
@@ -56,11 +68,46 @@ public class Timeline extends AppCompatActivity {
             }
         });
 
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            Toast.makeText(Timeline.this, "Latitude is"+location.getLatitude() +" Longitute is "+location.getLongitude(), Toast.LENGTH_LONG).show();
+
+                        }
+                        else
+                            Toast.makeText(Timeline.this, "Empty", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
         /*
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
          */
 
+                        }
+
+    private void requestPermission(){
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},1);
     }
+
 }
+
