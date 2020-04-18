46760b930aac23c534a6d57f01804a67f1c513d3
Amerah Al-gaidi <meesha1400@gmail.com>
2020-04-18 10:34:24 +0300

    categories method ?

--- app/src/main/java/com/example/ruzun/ejarahtest/categoriesAdapter.java
+++ app/src/main/java/com/example/ruzun/ejarahtest/categoriesAdapter.java
@@ -11,17 +11,32 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 
 public class categoriesAdapter extends ArrayAdapter<String> {
-    /* int count = 0 ;
+
+     int count = 0 ;
      FirebaseAuth mFirebaseAuth;
      FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
      DatabaseReference databaseReference = firebaseDatabase.getReference();
- */
     Context context;
     String rTitle[];
     int rImgs[];
     ProgressBar progressBar ;
+
+
+    String email;
+    User currentUser;
+
+    static ArrayList<Level> levels = new ArrayList<Level>();
 
     categoriesAdapter (Context c, String[] title, int[] imgs, ProgressBar bar  ) {
         super(c, R.layout.categories_level, R.id.textView1, title);
@@ -45,10 +60,69 @@ public class categoriesAdapter extends ArrayAdapter<String> {
 
         ImageView images = listItemView.findViewById(R.id.image);
         TextView myTitle = listItemView.findViewById(R.id.textView1);
-        ProgressBar progressBar1 = listItemView.findViewById(R.id.bar);
+        final ProgressBar progressBar1 = listItemView.findViewById(R.id.bar);
 
 
-        progressBar1.setProgress(10);
+
+
+        databaseReference.child("User").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    User user = snapshot.getValue(User.class);
+                    if(user.getEmail()!=null&&email!=null)
+                        if (user.getEmail().toLowerCase().equals(email.toLowerCase()))
+                        {
+                            currentUser = user;
+                        }
+                }
+                databaseReference.child("User").child(currentUser.getUserID()).child("level").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+
+                            Level level = snapshot.getValue(Level.class);
+                            levels.add(level);
+                            count =  level.getPoints();
+                            progressBar1.setProgress(count);
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+                            progressBar1.setMax(100);
+                            progressBar1.setProgress(level.getPoints());
+*/
+
+
+
+                        }
+
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         // now set our resources on views
